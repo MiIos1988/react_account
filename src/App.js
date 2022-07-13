713ea1a9725eb8b4be43c08643a75580256311de
Milos1988 <vojvoda1988>
2022-07-13 13:47:19 +0200

    dodan navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
 
 class App extends Component {
   state = {};
   render() {
-    return <h1>Radi</h1>;
+    return (
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
   }
 }
 
